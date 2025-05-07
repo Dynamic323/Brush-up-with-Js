d780db36f61f094cdda39aec031903f26d710100
DY <otamereisrael8@gmail.com>
2025-05-07 11:37:11 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// let
+// const
+// var
+
+// {
+//   let Name = "Israel";
+//   console.log(Name);
+// }
+
+// if (true) {
+//   var Name = "Israel";
+// }
+//   console.log(Name);
+
+// function User() {
+//   var Name = "Israel";
+// }
+
+// console.log(Name);
+// User()
+
+// var userName = "Israel Joel";
+
+// console.log(userName);
+
+// var userName = "Max";
+// console.log(userName);
+
+// let couter = "1";
+// couter = "2";
+
+// Data types in Js
+// string "" '' ``
+
+// let myname = "israel"
+// let myname = 'israel'
+let myname = `israel`;
+
+// Number
+let age = 25;
+
+// Boolean
+let isAdmin = true;
+
+// part that login user
+
+// if (isAdmin == true) {
+//   console.log("You can enter");
+// }
+
+// Array
+// let colors = ["green", "cars", "gold"];
+
+// console.log(colors[1][1]);
+
+// Object
+let userinfo = {
+  Name: "Daniel",
+  Password: "12we#4@@",
+};
+
+console.log(userinfo.Name);
