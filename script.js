af90128ce2b45071dffadfb3bb93e3137e7056ce
DY <otamereisrael8@gmail.com>
2025-05-12 12:10:41 +0100

    edited some stuff

--- script.js
+++ script.js
@@ -202,10 +202,10 @@ const facebookData = {
 //   console.log("Go home ");
 // }
 
-let x = 7;
-let y = 2;
-let res = x % y;
-console.log(x % y);
+// let x = 7;
+// let y = 2;
+// let res = x % y;
+// console.log(x % y);
 
 // if
 
@@ -259,7 +259,7 @@ console.log(x % y);
 //   console.log("You are not valid");
 // }
 
-let isAdmin = 12;
+// let isAdmin = 12;
 
 // if (isAdmin) {
 //   console.log("Cool");
@@ -272,3 +272,113 @@ let isAdmin = 12;
 // isAdmin ? console.log("cool") : console.log("Not cool");
 
 // string concatiination
+
+// let number = prompt("Enter a number from 1 to 30");
+
+// if (number < 1 || number > 30) {
+//   alert("Error invalid number");
+// }
+
+// let result = number % 2;
+
+// if (result == 0) {
+//   console.log("Even number");
+// }
+
+// if (result == 1) {
+//   console.log("Odd number");
+// }
+
+// let Num = 3;
+
+// switch (Num) {
+//   case 4:
+//     console.log("NUmber is 4");
+
+//     break;
+//   default:
+//     console.log("Invalid");
+
+//     break;
+// }
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let Name = "Max"
+// let Age= 11
+// let gender = "male"
+
+// console.log("Hello ${Name}");
+// let msg = "hello ${} you are ${}"
+// console.log(`Hello ${Name} you are ${age} yrs old`);
+
+let users = ["Max", "peter", "john", "israel", "josh"];
+
+// users.map(function (){
+
+// })
+
+users.map((item) => {
+  console.log(`Hello ${item}  you are welcome`);
+});
+
+let post_content = document.getElementById("post_content");
+
+const post = {
+  Heading: "How to make money online",
+  content:
+    "Enim eveniet sunt dolorum corrupti eum dolores expedita tenetur similique",
+  views: 20,
+  likes: [
+    {
+      name: " Dy",
+      reaction: 1,
+    },
+    {
+      name: "Pter",
+      reaction: 2,
+    },
+    {
+      name: " Daniel",
+      reaction: 3,
+    },
+  ],
+};
+
+const msg = `
+  <h1>${post.Heading} <h1/>
+  <p> ${post.content} <p/>
+
+  ${post.likes.map(() => {
+
+    ""
+  })}
+`;
+
+post_content.innerHTML = msg;
+
+// users.map)
+
+// function Dosmth(msg) {
+//   console.log(msg);
+// }
+
+// const Dosmth = (worker) =>{
+//   console.log(msg);
+// }
+
+// Dosmth("Hello User you are welcome");
+
+// const Dosmthh = ()
+
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// console.log(users[3]);
+// console.log(users.length);
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(users[i]);
+// }
